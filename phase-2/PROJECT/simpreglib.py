b3a9c64528d63e4b449705aa4ede2be404be405d
Zeth Abney <zethusabney@gmail.com>
2022-03-06 16:54:39 -0600

    more data viz exploration

--- phase-2/PROJECT/simpreglib.py
+++ phase-2/PROJECT/simpreglib.py
@@ -28,47 +28,66 @@ def reg_line (m, c, xs):
 def predict_y(m,c,x_new):
     return [m*x + c for x in x_new]
 
+def simpleregression(x,y,x_new):
+
+    m, c = best_fit(x,y)
+    regline = reg_line(m,c,x)
+    y_pred = predict_y(m,c,x_new)
+
+    return m,c,regline,y_pred
+
+# run a simple regression and plot results, x_new is optional
 def simpregplot(x,y,x_new=[]):
     
-    m, c = srl.best_fit(x,y)
-    regline = srl.reg_line(m,c,x)
-
-    y_pred = srl.predict_y(m,c,x_new)
+    m, c = best_fit(x,y)
+    regline = reg_line(m,c,x)
+    y_pred = predict_y(m,c,x_new)
 
     plt.scatter(x,y,color='blue')
     plt.plot(x,regline,color='gold')
     plt.scatter(x_new,y_pred,color='red')
+    plt.xlabel("Independent feature(X)")
+    plt.ylabel("Dependent Feature (Y)")
     plt.show()
 
 
-# Calculate sum of squared errors between regression and mean line 
-def sq_err(y_real, y_predicted):
-    """
-    input
-    y_real : true y values
-    y_predicted : regression line
+# # Calculate sum of squared errors between regression and mean line 
+# def sq_err(y_real, y_predicted):
+#     """
+#     input
+#     y_real : true y values
+#     y_predicted : regression line
 
     
-    return
-    squared error between regression and true line (ss_tot)
-    """
-    return round(np.sum((abs(y_real-y_predicted))**2),2)
+#     return
+#     squared error between regression and true line (ss_tot)
+#     """
+#     y_real = np.array(y_real)
+#     y_predicted = np.array(y_predicted)
+#     return round(np.sum((abs(y_real-y_predicted))**2),2)
 
-# Calculate Y_mean , squared error for regression and mean line , and calculate r-squared
-def r_squared(y_real, y_predicted):
-    """
-    input
-    y_real: real values
-    y_predicted: regression values
+# # Calculate Y_mean , squared error for regression and mean line , and calculate r-squared
+# def r_squared(y_real, y_predicted):
+#     """
+#     input
+#     y_real: real values
+#     y_predicted: regression values
     
-    return
-    r_squared value
-    """
-    real_bar = y_real.mean()
-    SSR = sq_err(y_real,y_predicted)
-    SST = np.sum((y_real-real_bar)**2)
+#     return
+#     r_squared value
+#     """
+#     y_real = np.array(y_real)
+#     y_predicted = np.array(y_predicted)
     
-    return round(1 - SSR/SST,2)
+#     real_bar = y_real.mean()
+#     SSR = sq_err(y_real,y_predicted)
+#     SST = np.sum((y_real-real_bar)**2)
+    
+#     return round(1 - SSR/SST,2)
+
+
+def QQ_plotter():
+    
 
 
 
