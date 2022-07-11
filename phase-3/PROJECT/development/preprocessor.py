5cf58cc1241d591bc72c2fab1762079a797f14bd
Zeth Abney <zethusabney@gmail.com>
2022-07-11 11:27:28 -0500

    post review revision complete

--- phase-3/PROJECT/development/preprocessor.py
+++ phase-3/PROJECT/development/preprocessor.py
@@ -9,6 +9,8 @@ from matplotlib.pyplot import axis, table
 import pandas as pd
 import numpy as np
 
+from sklearn.preprocessing import OneHotEncoder
+
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import EditedNearestNeighbours
 from imblearn.combine import SMOTEENN
@@ -44,7 +46,19 @@ def data_cleaner(file_path: str):
     X = df.drop('Customer Type',axis=1)
 
     # one-hot-encode categoricals within X
-    X = pd.get_dummies(X,drop_first=True)
+    # X = pd.get_dummies(X,drop_first=True)
+
+    X_ohe = OneHotEncoder(handle_unknown='ignore')
+    # slice columns to be encoded
+    objx_df = X[['Gender','Type of Travel','Class','satisfaction']]
+    # fit and transform encoder objext, convert to numpy array
+    objx_ohe = X_ohe.fit_transform(objx_df).toarray()
+    # get new X_ohe labels
+    objx_ohe_labels = [label[3:] for label in list(X_ohe.get_feature_names())]
+    # convert X_ohe array to dataframe include new labels
+    objx_ohe_df = pd.DataFrame(objx_ohe,columns=objx_ohe_labels)
+    # replace old cat columns with new encoded columns
+    X = pd.concat([X.drop(objx_df.columns,axis=1),objx_ohe_df],axis=1)
 
     # imputes any zeros in the survey as the mode of the surveyee's other answers.
     survey = X[survey_labels] #slice only the survey feature
@@ -61,8 +75,15 @@ def data_cleaner(file_path: str):
     # define y below
     y = df['Customer Type']
     # one-hot-encode target
-    y = pd.get_dummies(y,drop_first=True)['disloyal Customer'] # 1 = disloyal
-    
+    # y = pd.get_dummies(y,drop_first=True)['disloyal Customer']
+    # same steps as above but for the target feature
+    y_array = y.values.reshape(-1,1)
+    y_ohe = OneHotEncoder()
+    y_ohe_array = y_ohe.fit_transform(y_array).toarray()
+    y_ohe_labels = [label[3:] for label in list(y_ohe.get_feature_names())]
+    # values to convert back to numpy and ravel() to flatten the array, necessary to avoid convergence warnings
+    y = pd.DataFrame(y_ohe_array,columns=y_ohe_labels).drop('Loyal Customer',axis=1).values.ravel()
+
     return X,y
 
 # data resampling (SMOTEENN)
