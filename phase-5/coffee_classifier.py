46442621709aa6fa39f84e9faa0dae2a3d17a3ea
Zeth Abney <zethusabney@gmail.com>
2023-02-25 18:08:42 -0600

    POC now has AI prediction integrated, displays filepath and predicted label

--- phase-5/coffee_classifier.py
+++ phase-5/coffee_classifier.py
@@ -3,6 +3,55 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from keras.models import load_model
+
+coffee_vision = load_model('data/final_model.h5')
+
+def classify_bean(filepath:str):
+    """
+    Classifies the roast level of a coffee bean image.
+
+    Args:
+        filepath (str): The file path of the coffee bean image to classify.
+
+    Returns:
+         string: the predicted label as a string: "Green", "Light", "Medium" or "Dark".
+
+    Raises:
+        ValueError: If the input image is not a valid file or if the input image shape is not 224x224.
+    """
+
+    if not os.path.isfile(filepath):
+        print("Error: {} is not a valid file path.".format(filepath))
+        return
+
+    else:
+        # Load image and resize to model input shape
+        img = load_img(filepath, target_size=(224, 224))
+
+        # Convert image data to numpy array
+        img_array = img_to_array(img)
+
+        # Convert the image to grayscale
+        img_array = np.dot(img_array, [0.2989, 0.5870, 0.1140])
+
+        # Normalize the image array
+        img_array /= 255.0
+
+        # Expand dimensions to match the input shape of the model
+        input_array = np.expand_dims(img_array, axis=0)
+        input_array = np.expand_dims(input_array, axis=-1)
+
+        # Make predictions
+        predictions = coffee_vision.predict(input_array)
+        predicted_label = np.argmax(predictions)
+
+        label_dict = {0:"Green",1:"Light",2:"Medium",3:"Dark"}
+
+        return label_dict[predicted_label]
+
 
 class ImageLabel(QLabel):
     def __init__(self):
@@ -18,6 +67,7 @@ class ImageLabel(QLabel):
 
     def setPixmap(self, image):
         super().setPixmap(image)
+
 
 class AppDemo(QWidget):
     def __init__(self):
@@ -40,9 +90,28 @@ class AppDemo(QWidget):
         self.pathLabel = QLabel()
         mainLayout.addWidget(self.pathLabel)
 
+        # Create the label for the predicted label and add it to the layout
+        self.label = QLabel()
+        mainLayout.addWidget(self.label)
+
         # Set the layout for the window
         self.setLayout(mainLayout)
 
+    def classify_image(self, file_path):
+        try:
+            predicted_label = classify_bean(file_path)
+            self.label.setText("Predicted label: " + predicted_label)
+        except ValueError as e:
+            self.label.setText("Error: " + str(e))
+
+    def dragEnterEvent(self, event):
+        # Check if the event has image data and accept it if it does
+        if event.mimeData().hasImage:
+            event.accept()
+        else:
+            event.ignore()
+
+   
     def dragEnterEvent(self, event):
         # Check if the event has image data and accept it if it does
         if event.mimeData().hasImage:
@@ -63,6 +132,7 @@ class AppDemo(QWidget):
             event.setDropAction(Qt.CopyAction)
             file_path = event.mimeData().urls()[0].toLocalFile()
             self.set_image(file_path)
+            self.classify_image(file_path)
             # Set the text of the file path label to the dropped image's file path
             self.pathLabel.setText(file_path)
 
